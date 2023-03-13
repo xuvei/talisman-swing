f9a5022636c3fb842b7607473468cab3aeb0bc95
Sandra Lemańska <lemasank@gmail.com>
2023-03-13 14:38:41 +0100

    r: rendering pawn placement

--- src/main/java/org/example/client/ClientParams.java
+++ src/main/java/org/example/client/ClientParams.java
@@ -1,6 +1,10 @@
 package org.example.client;
 
 public class ClientParams {
-    public static final int WIDTH = 2560;
-    public static final int HEIGHT = 1440;
+    public static final int WIDTH = 2500;
+    public static final int HEIGHT = 1350;
+    public static final int FIELD_WIDTH = WIDTH / 7;
+    public static final int FIELD_HEIGHT = HEIGHT / 7;
+    public static final int PAWN_WIDTH = 50;
+    public static final int PAWN_HEIGHT = 50;
 }
--- src/main/java/org/example/client/View.java
+++ src/main/java/org/example/client/View.java
@@ -5,12 +5,15 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+import static org.example.client.ClientParams.*;
+
 public class View {
-    private BufferedImage bufferedImage;
+    public static final int PAWN_Y = FIELD_HEIGHT / 2;
+    private final Image boardImage;
 
     public View() {
         try {
-            bufferedImage = ImageIO.read(View.class.getResource("/talismanBoard.jpg"));
+            boardImage = ImageIO.read(View.class.getResource("/talismanHQ.jpg"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -21,12 +24,41 @@ public class View {
     }
 
     public void render(Graphics2D g) {
-        int fieldId = 1;
-        int fieldWidth = ClientParams.WIDTH / 7;
+        //dla pola o ID zaczytujemy boxa o danych wymiarach
+        int fieldId = 4;
+
+        int spacesAmountInLine = 7;
+        int lastFieldInLineIndex = spacesAmountInLine - 1;
 
         //g.drawRect(400, 450, 300, 350);
-        g.drawImage(bufferedImage, 0, 0, ClientParams.WIDTH, ClientParams.HEIGHT, null);
+        g.drawImage(boardImage, 0, 0, ClientParams.WIDTH, ClientParams.HEIGHT, null);
         g.setColor(Color.ORANGE.darker());
-        g.fillOval(fieldId * fieldWidth,100,100,100);
+        if (fieldId <= 6) {
+            g.fillOval(fieldId * FIELD_WIDTH + centerPawnModifierX(), PAWN_Y, PAWN_WIDTH, PAWN_HEIGHT);
+            return;
+        }
+
+        int steppesModificatierInLine = fieldId % lastFieldInLineIndex;
+        if (fieldId <= 12) {
+            g.fillOval(lastFieldInLineIndex * FIELD_WIDTH + centerPawnModifierX(),
+                    PAWN_Y + steppesModificatierInLine * FIELD_HEIGHT,
+                    PAWN_WIDTH, PAWN_HEIGHT);
+        } else if (fieldId <= 17) {
+            g.fillOval(lastFieldInLineIndex * FIELD_WIDTH + centerPawnModifierX() - steppesModificatierInLine * FIELD_WIDTH,
+                    lastFieldInLineIndex * FIELD_HEIGHT + centerPawnModifierY(),
+                    PAWN_WIDTH, PAWN_HEIGHT);
+        } else if (fieldId <= 23) {
+            g.fillOval(centerPawnModifierX(),
+                    lastFieldInLineIndex * FIELD_HEIGHT + centerPawnModifierY() - steppesModificatierInLine * FIELD_HEIGHT,
+                    PAWN_WIDTH, PAWN_HEIGHT);
+        }
+    }
+
+    private int centerPawnModifierX() {
+        return FIELD_WIDTH / 2 - PAWN_WIDTH / 2;
+    }
+
+    private int centerPawnModifierY() {
+        return FIELD_HEIGHT / 2 - PAWN_HEIGHT / 2;
     }
 }
